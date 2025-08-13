1d536112b573512e2059e23937a5c9211a0ff74e
akgmx <114804861+akgmx@users.noreply.github.com>
2025-08-13 22:43:56 +0545

    modified:   middleware/authguard.js

--- middleware/authguard.js
+++ middleware/authguard.js
@@ -4,7 +4,6 @@ const JWT_SECRET = process.env.JWT_TOKEN;
 const authGuard = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    console.log(authHeader);
     return res
       .status(401)
       .json({ success: false, message: "Authentication token missing" });
